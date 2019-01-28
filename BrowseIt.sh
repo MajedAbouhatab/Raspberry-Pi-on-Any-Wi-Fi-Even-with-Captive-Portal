00bf287451b79fc064d5ea581ace935ca34a6e14
Majed Abouhatab, P.E <45211996+MajedAbouhatab@users.noreply.github.com>
2019-01-28 13:23:03 -0600

    Update BrowseIt.sh

--- BrowseIt.sh
+++ BrowseIt.sh
@@ -1,5 +1,7 @@
-chromium-browser 8.8.8.8 & #192.168.1.1 &
+chromium-browser 8.8.8.8 & #192.168.1.1 & #google.com &
 sleep 60
 xdotool key Tab
 sleep 1
 xdotool key KP_Enter
+#xdotool key space
+#xdotool type Majed Abouhatab, P.E.
